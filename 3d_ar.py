a3938b880db3009cec90509fb75bc05fc2fcb4e5
abhivansh <148888212+avg16@users.noreply.github.com>
2024-12-08 10:53:05 +0530

    objload

--- 3d_ar.py
+++
@@ -1,7 +0,0 @@
-#ar script here
-import cv2
-import numpy as np
-import pandas as pd
-import sys
-import os
-import matplotlib.pyplot as plt
---
+++ objloader.py
@@ -0,0 +1,47 @@
+#ar script here
+class OBJ():
+    def __init__(self, filename, swap_y_z = False):
+
+        self.faces = []
+        self.texture_coords = []
+        self.normals = []
+        self.vertices = []
+        material = None
+
+        for line in open(filename, "r"):
+            if line.startswith('#'): 
+                continue #if any comment comes in b/w
+            values = line.split()
+            if not values: 
+                continue
+            if values[0] == 'v': # vertex
+                v = map(float, values[1:4])
+                if swap_y_z== True:
+                    v = v[0], v[2], v[1]
+                self.vertices.append(v)
+            elif values[0] == 'vt': # texture coordinate
+                v = map(float, values[1:3])
+                self.texture_coords.append(v)
+            elif values[0] == 'vn': # normal
+                v = map(float, values[1:4])
+                if swap_y_z==True:
+                    v = v[0], v[2], v[1]
+                self.normals.append(v)
+            # elif values[0] == 'usemtl': # new material
+            #     material = values[1]
+            elif values[0] == 'f': # face
+                face = []
+                texcoords = []
+                norms = []
+                for v in values[1:]:
+                    w = v.split('/')
+                    face.append(int(w[0]))
+                    if len(w) >= 2 and len(w[1]) > 0:
+                        texcoords.append(int(w[1]))
+                    else:
+                        texcoords.append(0)
+                    if len(w) >= 3 and len(w[2]) > 0:
+                        norms.append(int(w[2]))
+                    else:
+                        norms.append(0)
+                self.faces.append((face, texcoords, norms))
